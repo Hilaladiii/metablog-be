20693e74325cd59db2e7abd440b37e43318333ce
Hilaladiii <abdulhilal242@gmail.com>
2024-10-24 22:27:25 +0700

    feat : add all module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,28 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { PostModule } from './modules/post/post.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { RoleGuard } from './common/guards/role.guard';
+import { TokenModule } from './providers/token/token.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, PostModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UserModule,
+    PostModule,
+    AuthModule,
+    TokenModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule {}
