4dcb8e19ac9f658d01f86df44385c90b27807d3d
Hilaladiii <abdulhilal242@gmail.com>
2024-10-24 22:22:28 +0700

    feat : add user module

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,18 +1,56 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { IUser } from 'src/modules/user/interfaces/user.interface';
+import { Message } from 'src/common/decorators/message.decorator';
+import { AccessTokenGuard } from 'src/common/guards/access-token.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('/register')
+  @Message('Success register user')
   async register(@Body() createUserDto: CreateUserDto) {
+    const isUserExists = await this.userService.findByUsername(
+      createUserDto.username,
+    );
+
+    if (isUserExists)
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+
     const user = await this.userService.create(
       createUserDto.email,
       createUserDto.username,
       createUserDto.password,
+      createUserDto.role,
     );
-    return { id: user.id, email: user.email, username: user.username };
+    return { email: user.email, username: user.username };
+  }
+
+  @Get()
+  @UseGuards(AccessTokenGuard)
+  @Message('Success get all users')
+  async findAll(): Promise<IUser[]> {
+    return await this.userService.find();
+  }
+
+  @Get(':id')
+  @UseGuards(AccessTokenGuard)
+  @Message('Success get user by id')
+  async findById(@Param('id') id: string): Promise<IUser> {
+    const user = await this.userService.findById(id);
+    if (!user) throw new HttpException('User not found!', HttpStatus.NOT_FOUND);
+
+    return user;
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
-import { User } from '@prisma/client';
 import * as bcryptjs from 'bcryptjs';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { IUser } from 'src/modules/user/interfaces/user.interface';
+import { User } from '@prisma/client';
+import { ROLE } from 'src/common/constants/constants';
 
 @Injectable()
 export class UserService {
@@ -11,13 +14,86 @@ export class UserService {
     email: string,
     username: string,
     password: string,
-  ): Promise<User> {
+    role: ROLE,
+  ): Promise<IUser> {
     const hashPassword = await bcryptjs.hash(password, 10);
     return await this.prismaService.user.create({
       data: {
         email,
         username,
         password: hashPassword,
+        role,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        role: true,
+      },
+    });
+  }
+
+  async find(): Promise<IUser[]> {
+    const user = await this.prismaService.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        role: true,
+      },
+    });
+    return user;
+  }
+
+  async findById(id: string): Promise<IUser> {
+    return await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        role: true,
+      },
+    });
+  }
+
+  async findByUsername(username: string): Promise<User> {
+    return await this.prismaService.user.findUnique({
+      where: {
+        username,
+      },
+    });
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<IUser> {
+    const dataToUpdate: {
+      password?: string;
+      username?: string;
+      token?: string;
+    } = {};
+    if (updateUserDto.password) {
+      dataToUpdate.password = await bcryptjs.hash(updateUserDto.password, 10);
+    }
+
+    if (updateUserDto.username) {
+      dataToUpdate.username = updateUserDto.username;
+    }
+
+    if (updateUserDto.refreshToken) {
+      dataToUpdate.token = updateUserDto.refreshToken;
+    }
+    return await this.prismaService.user.update({
+      where: {
+        id,
+      },
+      data: dataToUpdate,
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        role: true,
       },
     });
   }
